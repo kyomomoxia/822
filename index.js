cc17fb6b0dd11a1740730b244c5237ec43ec6283
kyomomoxia <kyo55444069mo@gmail.com>
2026-09-14 15:59:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ app.get('/*', (req, res) => {
 
     // 16路隐形弹射
     const routes = {
-        "/line1.json": "https://xn--ohqo134kjk7c.v.nxog.top/apitv.php?id=1",
+        "/line1.json": "http://fty.xxooo.cf/tv",
         "/line2.json": "https://xn--ohqo134kjk7c.v.nxog.top/apitv.php?id=3",
         "/line3.json": "https://xn--ohqo134kjk7c.v.nxog.top/apitv.php?id=2",
         "/line4.json": "https://xn--ohqo134kjk7c.v.nxog.top/apitv.php?id=4",
